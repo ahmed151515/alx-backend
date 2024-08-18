e37dcd4659c7633927faca9e007d953b3b03cb38
ahmed151515 <ahmedarafa.20444@gmail.com>
2024-08-18 21:07:12 +0300

    .

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """solve task"""
-from typing import Tuple, Union
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[Union[int, int]]:
+def index_range(page: int, page_size: int) -> Tuple:
+    """fun"""
     s = (page - 1) * page_size
     return (1, s + page_size)
