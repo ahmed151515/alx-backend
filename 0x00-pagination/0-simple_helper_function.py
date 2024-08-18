30b9b410e132fd2036958e160a8d3dde6a907210
ahmed151515 <ahmedarafa.20444@gmail.com>
2024-08-18 20:24:20 +0300

    .

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """solve task"""
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple:
     s = (page - 1) * page_size
     return (1, s + page_size)
